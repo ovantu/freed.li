511dd390b1b2b5b8c16fe573539c5df72b898f3b
Luis Prill Sempere <luisps@web.de>
2013-12-26 19:44:28 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wispper::Application.config.secret_key_base = 'b16b88c1a626b9f6b057f140e147865801a8a07d781b5a2923205cd9b7eb53b3138c61f9f80979573dfd3564308e587af51821b64e339e0eebc11385b3d46e63'

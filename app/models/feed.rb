f794a850dcd00ca7a64d88d9dfb855c3945271fd
Luis Prill Sempere <luisps@web.de>
2014-01-18 02:27:12 +0100

    changed freeds index and a bit show

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,8 @@
 class Feed < ActiveRecord::Base
   has_many :posts, :class_name => "Post", :foreign_key => "feed_id"
+  
+  def contributors
+    posts.select(:creator_id).distinct
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Wispper::Application.routes.draw do
   resources :posts
-  get "posts/new/:feed_id" => "posts#new", :as => "new_post_for_feed"
+  get "freeds/:feed_id/posts/new" => "posts#new", :as => "new_post_for_feed"
 
-  resources :feeds
+  resources :feeds, :path => "freeds"
 
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}

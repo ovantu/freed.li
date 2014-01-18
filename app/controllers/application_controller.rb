7c81f42994c2421dc95719f101e404bd28876e0c
Luis Prill Sempere <luisps@web.de>
2014-01-18 02:57:04 +0100

    added httpaccept and languages in routes etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+  before_action :set_locale
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,5 +8,19 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  # reads out the params locale automatically out of URL or browser setting and compare to the available languages
+  def set_locale
+    available = %w(en de es)
+    I18n.locale = params[:locale] || http_accept_language.compatible_language_from(available)
+   # I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
+  # Automatic setting and insertion of locale in link generation
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { locale: I18n.locale }
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Wispper::Application.routes.draw do
-  resources :posts
-  get "freeds/:feed_id/posts/new" => "posts#new", :as => "new_post_for_feed"
-
-  resources :feeds, :path => "freeds"
-
-  root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  # for niçe URL with locale as parameter. locale is not necesary and goes first to en than to de
+  scope "(:locale)", locale: /en|de|es/ do
+    resources :posts
+    resources :feeds, :path => "freeds"
+    root :to => "home#index"
+    get "freeds/:feed_id/posts/new" => "posts#new", :as => "new_post_for_feed"
+    devise_for :users, :controllers => {:registrations => "registrations"}
+    resources :users
+  end
 end

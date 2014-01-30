c5b0a6388e1308e207126dae91da32f4bce4ee85
Luis Prill Sempere <luisps@web.de>
2014-01-30 17:13:43 +0100

    added authenticated toutes

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,11 @@
 class Feed < ActiveRecord::Base
   has_many :posts, :class_name => "Post", :foreign_key => "feed_id"
+  validates :goal, length: { in: 10..160 }
+  validates :rule1, length: { in: 10..160 }
+  validates :rule2, length: { in: 0..160 }
+  validates :rule3, length: { in: 0..160 }
+  validates :rule4, length: { in: 0..160 }
+  validates :rule5, length: { in: 0..160 }
   
   def contributors
     posts.select(:creator_id).distinct
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Wispper::Application.routes.draw do
   # for niçe URL with locale as parameter. locale is not necesary and goes first to en than to de
   scope "(:locale)", locale: /en|de|es/ do
+    authenticated :user do
+        # Rails 4 users must specify the 'as' option to give it a unique name
+        root :to => "feeds#index", :as => "authenticated_root"
+      end
     resources :posts
     resources :feeds, :path => "freeds"
     root :to => "home#index"

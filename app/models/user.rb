9bda62dc78af349da663a0e69be137472e749a3e
Luis Prill Sempere <luisps@web.de>
2014-02-04 09:27:10 +0100

    fixed User.feeds and .posts relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,29 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_many :feeds, :class_name => "feed", :foreign_key => "creator_id"
-  has_many :posts, :class_name => "post", :foreign_key => "creator_id"  
+  has_many :feeds, :class_name => "Feed", :foreign_key => "creator_id"
+  has_many :posts, :class_name => "Post", :foreign_key => "creator_id"  
   has_many :evaluations    
   has_many :eval_posts, :through => :evaluations, :source => :post
   
   scope :rated_by, -> (user_id){joins(:questions_ratings).where(questions_ratings:{creator_id: user_id})}
   
+  # shows all evaluations which are still pending
   def pending_evaluations
     e = evaluations.where(status: "pending")
   end
   
+  # shows all posts that still have to be evaluated
   def all_posts_to_be_evaluated
     e = eval_posts.where(evaluations:{status: "pending"})
   end
   
+  # shows all posts for a FEED that haven't been evaluated
   def posts_to_be_evaluated_in_feed(feed_id)
     e = eval_posts.where(evaluations:{status: "pending"}).where(feed_id: feed_id)
   end
   
+  # shows all posts of a FEED that are in_evaluation
   def posts_in_evaluation(feed_id)
     e = eval_posts.where(status: "in_evaluation").where(feed_id: feed_id)
   end
